404c936461783133bcca20cb6dc727126f5c2870
maurohcardona <maurohcardona@gmail.com>
2023-01-19 19:55:53 -0300

    va queriendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
 import './App.css';
+import { useState } from 'react';
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const TURNS = {
+  X:'x',
+  O:'o'
+}
 
 function App() {
-  return (
-    <h1>Tatetiddd</h1>
+ 
+ const [board, setBoard] = useState(Array(9).fill(null))
+ const [turn, setTurn] = useState(TURNS.X)
+  
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+  }
 
+
+  return (
+    <main className='board'>
+      <h1>Tateti</h1>
+      <section className='game'>
+        {
+          board.map((_, index) =>{
+            return(
+             <Square 
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+                >
+                  {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className='turn'>
+        <Square isSelected={ turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={ turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
   );
 }
 
