9882b0b389fbf7708f52f2f8b8c2b1050ee1983a
maurohcardona <maurohcardona@gmail.com>
2023-01-21 17:31:06 -0300

    seteando winner

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
   return(
     <div onClick={handleClick} className={className}>
@@ -19,15 +19,52 @@ const TURNS = {
   O:'o'
 }
 
+const winnerCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
  
  const [board, setBoard] = useState(Array(9).fill(null))
  const [turn, setTurn] = useState(TURNS.X)
-  
-  const updateBoard = () => {
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-  }
+ const [winner, setWinner] =useState(null)
 
+ const checkWinner = (boardToCheck) => {
+  for (const combo of winnerCombos) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ){
+      return boardToCheck[a]
+    }
+ }
+   return null
+}
+  
+  const updateBoard = (index) => {
+    if(board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
+  }
 
   return (
     <main className='board'>
@@ -55,6 +92,27 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false
+                   ? 'Empate'
+                    : 'gano:'
+                }
+              </h2>
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+              <footer>
+                <button>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   );
 }
